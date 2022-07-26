3da60f4b86586e260ff07e960e7d50a37040488e
Batuhan Mutlu <batuhan.mutlu.0566@gmail.com>
2022-07-27 00:04:34 +0300

    module EDA created

--- EDA.py
+++ EDA.py
@@ -1,3 +1,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+def check_df(dataframe, head=5):
+    """
+    Returns general information about the data frame.
+    :param dataframe: The data frame whose information we want.
+    :param head: the number of data we want to observe starting from the beginning(default head = 5)
+    :return: no return
+    """
