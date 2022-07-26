3fc01a05d7940492cfd104a3fc9e5ee14afbd465
Batuhan Mutlu <batuhan.mutlu.0566@gmail.com>
2022-07-27 00:13:16 +0300

    module EDA created

--- EDA.py
+++ EDA.py
@@ -15,3 +15,8 @@ def check_df(dataframe, head=5):
     print(dataframe.dtypes)
     print("##################### Head #####################")
     print("##################### Tail #####################")
+    print(dataframe.tail(head))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.describe([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
