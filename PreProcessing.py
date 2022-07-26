21cfdc0e1357763b33b8261fe3577b1cb10953d9
Batuhan Mutlu <batuhan.mutlu.0566@gmail.com>
2022-07-27 00:39:28 +0300

    module PreProcessing created

--- PreProcessing.py
+++ PreProcessing.py
@@ -137,3 +137,5 @@ def remove_outlier(dataframe, col_name):
     """
 
     low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+
+    df_without_outliers = dataframe[~((dataframe[col_name] < low_limit) | (dataframe[col_name] > up_limit))]
