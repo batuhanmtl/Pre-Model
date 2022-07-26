6a079fbedd824cd1331ce85313cc126dfbf291ba
Batuhan Mutlu <batuhan.mutlu.0566@gmail.com>
2022-07-27 00:28:09 +0300

    module PreProcessing created

--- PreProcessing.py
+++ PreProcessing.py
@@ -42,3 +42,4 @@ def check_outlier(dataframe, col_name):
     low_limit, up_limit = outlier_thresholds(dataframe, col_name)
 
     if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
