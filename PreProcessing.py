65bab3db0ee6aa3c79903fa00b8fbcf6ce27adcc
Batuhan Mutlu <batuhan.mutlu.0566@gmail.com>
2022-07-27 00:25:35 +0300

    module PreProcessing created

--- PreProcessing.py
+++ PreProcessing.py
@@ -3,3 +3,14 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 # Outlier Thresholds
+
+def outlier_thresholds(dataframe, col_name, q1=0.25, q3=0.75):
+    """
+    Calculates a range for outliers.
+    :param dataframe: Pandas.DataFrame
+    :param col_name:  string
+    Variable name whose outliers will be determined
+    :param q1: quantile 1 int or float (optional default 0.25)
+    :param q3: quantile 3 int or float (optional default 0.75)
+    :return: int or float , low limit and up limit
+    """
