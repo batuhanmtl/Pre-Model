328b391a3e291d4abd460b30514498e44066686d
Batuhan Mutlu <batuhan.mutlu.0566@gmail.com>
2022-07-30 14:12:11 +0300

    General Update

--- PreProcessing.py
+++ PreProcessing.py
@@ -72,21 +72,17 @@ def grab_col_names(dataframe, cat_th=10, car_th=20):
     """
 
     # cat_cols, cat_but_car
-    cat_cols = [col for col in dataframe.columns if str(dataframe[col].dtypes) in ["category", "object", "bool"]]
-
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
     num_but_cat = [col for col in dataframe.columns if
-                   dataframe[col].nunique() < 10 and dataframe[col].dtypes in ["int", "float"]]
-
+                   dataframe[col].nunique() < cat_th and dataframe[col].dtypes != "O"]
     cat_but_car = [col for col in dataframe.columns if
-                   dataframe[col].nunique() > 20 and str(dataframe[col].dtypes) in ["category", "object"]]
-
+                   dataframe[col].nunique() > car_th and dataframe[col].dtypes == "O"]
     cat_cols = cat_cols + num_but_cat
-
     cat_cols = [col for col in cat_cols if col not in cat_but_car]
 
-    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes in ["int", "float"]]
-
-    num_cols = [col for col in num_cols if col not in cat_cols]
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
 
     print(f"Observations: {dataframe.shape[0]}")
     print(f"Variables: {dataframe.shape[1]}")
